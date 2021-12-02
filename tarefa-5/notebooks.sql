ca6a746608b40a9bb4e9fee1cd3e15d8640da673
PedroZaffalon <pedrozaffalondasilva@gmail.com>
2021-12-02 18:44:43 -0300

    atualização na tarefa 5

--- tarefa-5/notebooks.sql
+++ tarefa-5/notebooks.sql
@@ -6,22 +6,9 @@ CREATE SCHEMA lojas_notebook;
 
 CREATE TABLE lojas_notebook.notebook(
     nome CHAR(200) NOT NULL,
-    CONSTRAINT pk_notebook PRIMARY KEY(nome)
-);
-
-CREATE TABLE lojas_notebook.produto(
-    id SERIAL,
-    nome_loja CHAR(50),
-    preço NUMERIC(6, 2),
-    disponibilidade BOOL,
-    notebook_nome CHAR(200),
-    CONSTRAINT pk_produto PRIMARY KEY(id),
-    CONSTRAINT fk_notebook FOREIGN KEY(notebook_nome)
-        REFERENCES lojas_notebook.notebook(nome)
-);
-
-CREATE TABLE lojas_notebook.especificacoes_tecnicas(
-    notebook_nome CHAR(200),
+    cor CHAR(20),
+    tamanho_tela FLOAT,
+    Peso FLOAT,
     sistema_operacional CHAR(40),
     placa_de_video CHAR(60),
     ssd INT,
@@ -29,17 +16,26 @@ CREATE TABLE lojas_notebook.especificacoes_tecnicas(
     resolucao_da_tela CHAR(15),
     processador CHAR(20),
     memoria_ram INT,
-    CONSTRAINT pk_especificacoes PRIMARY KEY(notebook_nome),
-    CONSTRAINT fk_notebook FOREIGN KEY(notebook_nome)
-        REFERENCES lojas_notebook.notebook(nome) 
+    CONSTRAINT pk_notebook PRIMARY KEY(nome)
 );
 
-CREATE TABLE lojas_notebook.caracteristicas_fisicas(
-    cor CHAR(20),
-    tamanho_tela FLOAT,
-    Peso FLOAT,
+CREATE TABLE lojas_notebook.loja(
+    nome_loja CHAR(20),
+    url_site CHAR(40),
+    CONSTRAINT pk_loja PRIMARY KEY(nome_loja)
+)
+
+CREATE TABLE lojas_notebook.produto(
+    id SERIAL,
+    nome_notebook CHAR(50),
+    nome_loja CHAR(50),
+    classificacao FLOAT,
+    preço NUMERIC(6, 2),
+    disponibilidade BOOL,
     notebook_nome CHAR(200),
-    CONSTRAINT pk_caracteristicas_fisicas PRIMARY KEY(notebook_nome),
-    CONSTRAINT fk_notebook FOREIGN KEY(notebook_nome)
-        REFERENCES lojas_notebook.notebook(nome)
+    CONSTRAINT pk_produto PRIMARY KEY(id),
+    CONSTRAINT fk_notebook FOREIGN KEY(nome_notebook)
+        REFERENCES lojas_notebook.notebook(nome),
+    CONSTRAINT fk_loja FOREIGN KEY(nome_loja)
+        REFERENCES lojas_notebook.loja(nome)
 );
